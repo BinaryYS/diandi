3ff11f9a304da2e0462af9e0fd3b1339cacc5bcd
BinaryYS@gmail.com <20125061yang>
2018-05-14 23:36:57 +0800

    add  guava Object...

---
+++ src/main/java/com/ssg/cqu/guava/ObjectTest.java
@@ -0,0 +1,25 @@
+package com.ssg.cqu.guava;
+
+import com.google.common.base.Objects;
+import com.ssg.cqu.guava.model.Student;
+
+/**
+ * author : yangsong
+ * time   : 2018/5/14 23:18
+ * e-mail : binaryys@gmail.com
+ */
+public class ObjectTest {
+    public static void main(String[] args) {
+        Student s1 = new Student("Mahesh", "Parashar", 1, "VI");
+        Student s2 = new Student("Suresh", null, 3, null);
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.hashCode());
+        System.out.println(
+                Objects.toStringHelper(s1)
+                        .add("Name",s1.getFirstName()+" " + s1.getLastName())
+                        .add("Class", s1.getClassName())
+                        .add("Roll No", s1.getRollNo())
+                        .toString());
+    }
+}
